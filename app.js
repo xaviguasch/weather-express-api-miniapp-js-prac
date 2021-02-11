f0a8d3e7c97deb564b1c9acbf820288fb8493d39
xaviguasch <xaviguasch@gmail.com>
2021-02-11 13:13:36 +0100

    remove gitignore files

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+// const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', function (req, res) {
+  res.send(`lalallaa`)
+})
+
+app.listen(3000, function () {
+  console.log('server is running on port 3000')
+})
