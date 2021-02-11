562a9b2dff9c28fa8244627e3c567b6f162d291a
xaviguasch <xaviguasch@gmail.com>
2021-02-11 15:10:23 +0100

    rendered API data with express, live

--- app.js
+++ app.js
@@ -14,10 +14,15 @@ app.get('/', function (req, res) {
       const weatherData = JSON.parse(data)
       const temp = weatherData.main.temp
       const weatherDescription = weatherData.weather[0].description
-      console.log(weatherDescription)
+      const icon = weatherData.weather[0].icon
+      const imageURL = `http://openweathermap.org/img/wn/${icon}.png`
+
+      res.write(`<p>The weather is currenty ${weatherDescription}</p>`)
+      res.write(`<h1>The temperature in London is ${temp} degrees Celcius.</h1>`)
+      res.write(`<img src="${imageURL}" alt="">`)
+      res.send()
     })
   })
-  res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/', function (req, res) {
